fb3eeb786a6b912e289a6fe2044bebf9e9cb6ce4
v-fedorov <vfedorov@netflexity.com>
2016-05-20 12:17:55 -0400

    show price

--- app/modules/fare/fareCtrl.js
+++ app/modules/fare/fareCtrl.js
@@ -27,6 +27,29 @@
 
 			vm.selectedZone;
 			vm.selectedType;
+			vm.selectedPurchase;
+			vm.trips;
+
+			vm.currentZone = function() {
+				return _.find(vm.data.zones, { 'name': vm.selectedZone });
+			};
+
+			vm.getPrice = function() {
+				if (vm.selectedZone && vm.selectedPurchase && vm.trips) {
+					var zone = vm.currentZone();
+
+					// Update purchase type since 'Anytime' has only one option
+					vm.purchases = lodash.uniq(
+						_.map(_.filter(zone.fares, { 'type': vm.selectedType }), 'purchase')
+					);
+
+					console.log(vm.purchases)
+
+					vm.fare = _.find(zone.fares, { 'type': vm.selectedType, 'purchase': vm.selectedPurchase });
+					vm.price = vm.fare.price * vm.trips;
+					console.log(vm.price)
+				}
+			};
 
 			vm.getSelectedText = function(name) {
 				if (vm['selected' + name] !== undefined) {
@@ -38,17 +61,23 @@
 
 			vm.selectZone = function() {
 				// Get fares from selected zone
-				var zone = _.find(vm.data.zones, { 'name': vm.selectedZone });
+				var zone = vm.currentZone();
 
 				vm.types = lodash.uniq(
-						lodash.map(zone.fares, 'type')
+						_.map(zone.fares, 'type')
 					);
+
+				vm.purchases = lodash.uniq(
+						_.map(zone.fares, 'purchase')
+					);
+
+				vm.trips = 1;
 			};
 
 			// Intial data load, setting vars only after promise resolves
 			FareService.getData().then(function(data) {
 				vm.data  = data;
-				vm.zones = lodash.map(vm.data.zones, 'name');
+				vm.zones = _.map(vm.data.zones, 'name');
 				vm.info  = vm.data.info;
 				vm.types = [];
 			});
