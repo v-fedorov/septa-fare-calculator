169de9d4cf60edf0568c58a3bb08fa2d49f0a64b
v-fedorov <vfedorov@netflexity.com>
2016-05-20 11:16:46 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc index
+	* @name app
+	* @description
+	* # app
+	*
+	* Main module of the application.
+	*/
+
+	angular.module('septa-fare-calculator', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngMessages',
+		'ui.router',
+		'home',
+	]);
+
+})();
